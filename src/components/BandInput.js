d35383aef2ee69d016387bb03b935325ca0fb6d5
Heather Christman <efl7a@yahoo.de>
2018-08-03 15:28:03 -0400

    The first three BandInput tests would not pass.  I kept gettingt he error:Invariant Violation: Could not find 'store' in either the context or props of 'Connect(BandInput)'. Either wrap the root component in a <Provider>, or explicitly pass 'store' as a prop to Connect(BandInput)

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,43 @@
 // Add BandInput component
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class BandInput extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: "",
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addBand(this.state)
+    this.setState({
+      name: "",
+    });
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} value={this.state.name}></input>
+          <input type="submit"/>
+        </form>
       </div>
     )
   }
 }
 
-export default BandInput
+const mapDispatchToProps = dispatch => ({
+  addBand: formData => dispatch({ type: 'ADD_BAND', payload: formData})
+})
+
+export default connect(null, mapDispatchToProps)(BandInput)
